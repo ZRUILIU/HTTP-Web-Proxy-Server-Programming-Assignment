94cea92e197d2257da203f7cf91ca09061486e4a
ZRUILIU <zhongrui.liu@outlook.com>
2025-03-28 19:49:07 +1030

    step 7 Connect to Origin Server, and Create Request to Origin Server

--- Proxy-skeleton(2025).py
+++ Proxy-skeleton(2025).py
@@ -139,6 +139,7 @@ while True:
       address = socket.gethostbyname(hostname)
       # Connect to the origin server
       # ~~~~ INSERT CODE ~~~~
+      originServerSocket.connect((address, 80))
       # ~~~~ END CODE INSERT ~~~~
       print ('Connected to origin Server')
 
@@ -149,6 +150,8 @@ while True:
       # originServerRequest is the first line in the request and
       # originServerRequestHeader is the second line in the request
       # ~~~~ INSERT CODE ~~~~
+      originServerRequest = f"GET {resource} HTTP/1.1"
+      originServerRequestHeader = f"Host: {hostname}\r\nConnection: close"
       # ~~~~ END CODE INSERT ~~~~
 
       # Construct the request to send to the origin server
